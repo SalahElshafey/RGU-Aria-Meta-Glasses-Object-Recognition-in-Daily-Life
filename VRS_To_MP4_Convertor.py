59d46f0faceaaf9e6da8ac35289e4d981ad07755
Salah Elshafey <112121144+SalahElshafey@users.noreply.github.com>
2024-09-10 14:21:04 +0100

    Create VRS_To_MP4_Convertor.py

---
+++ VRS_To_MP4_Convertor.py
@@ -0,0 +1,3 @@
+from projectaria_tools.utils.vrs_to_mp4_utils import convert_vrs_to_mp4
+
+convert_vrs_to_mp4("/home/salah-elshafey/Downloads/3-Salah.vrs","/home/salah-elshafey/Downloads/3-Salah.mp4")
